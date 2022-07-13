67d06733f6df2ef12cdb5f3416053712f0bd4517
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-13 21:56:56 +0700

    Create ColorConstant.swift to store color values of RMIT

---
+++ ContactList/ColorConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ColorConstants.swift
+//  ContactList
+//
+//  Created by Tom Huynh on 7/13/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorConstants{
+    static let rmitBlue = Color(red: 0.03, green: 0.03, blue: 0.35)
+    static let rmitRed = Color(red: 0.90, green: 0.00, blue: 0.16)
+}

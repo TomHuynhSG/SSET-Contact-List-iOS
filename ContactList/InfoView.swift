ff8bd32c6a35984d2b6d78b5eb39660c0d226dff
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-13 21:58:25 +0700

    Complete the InfoView

---
+++ ContactList/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  ContactList
+//
+//  Created by Tom Huynh on 7/13/22.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50.0)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(ColorConstants.rmitRed)
+                Text(text)
+                    .foregroundColor(.black)
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}

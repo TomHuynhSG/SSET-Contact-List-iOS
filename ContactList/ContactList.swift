0bc515aabc758d596fc7ab26ac5713b35996cdd7
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-13 22:32:04 +0700

    Add basic display of ContactRow

---
+++ ContactList/ContactList.swift
@@ -0,0 +1,20 @@
+//
+//  ContactList.swift
+//  ContactList
+//
+//  Created by Tom Huynh on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContactList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactList_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactList()
+    }
+}

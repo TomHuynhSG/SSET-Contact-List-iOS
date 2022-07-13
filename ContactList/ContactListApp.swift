9eb204ac36e0c61118a34cb0c715e6e9a039d256
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-13 21:55:05 +0700

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Tom Huynh on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

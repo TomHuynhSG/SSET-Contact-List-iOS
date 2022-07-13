4d3840424373bc44e8adc7c11c3c76826c15402c
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-13 22:00:48 +0700

    - Add image assets of rmit logo and Tom's photo - Create ContactCard view

---
+++ ContactList/ContactCard.swift
@@ -0,0 +1,47 @@
+//
+//  ContactCard.swift
+//  ContactList
+//
+//  Created by Tom Huynh on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContactCard: View {
+    var body: some View {
+        
+        ZStack {
+            ColorConstants.rmitBlue.edgesIgnoringSafeArea(.all)
+                
+            VStack {
+                Image("rmit").resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.bottom, 20.0)
+                    .frame(width: 200)
+                Image("tom")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color(.white),lineWidth: 5))
+                Text("Tom Huynh")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+84 123 456 789", imageName: "phone.fill")
+                InfoView(text: "tom.huynh@rmit.edu.vn", imageName: "envelope.fill")
+            }
+        }
+        
+    }
+}
+
+struct ContactCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactCard()
+    }
+}

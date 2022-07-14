7ee6769d5908270825d930cc30bd945fb747fea8
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-15 00:02:31 +0700

    Change the coordination to RMIT Vietnam campus

--- ContactList/Model/ModelData.swift
+++ ContactList/Model/ModelData.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreLocation
 
 var contacts: [Contact] = [
-    Contact(name: "Tom Huynh", email: "tom.huynh@rmit.edu.vn", phone: "0912325232", imageName: "tom-huynh", locationCoordinate: CLLocationCoordinate2D(latitude: 10.804983484055525, longitude: 106.68265289036171)),
+    Contact(name: "Tom Huynh", email: "tom.huynh@rmit.edu.vn", phone: "0912325232", imageName: "tom-huynh", locationCoordinate: CLLocationCoordinate2D(latitude: 10.729410965174186, longitude: 106.69522548892152)),
     Contact(name: "Brett Kirk", email: "brett.kirk@rmit.edu.vn", phone: "09435325634", imageName: "brett-kirk", locationCoordinate: CLLocationCoordinate2D(latitude: 10.758256325746386, longitude: 106.67228491141948)),
     Contact(name: "Minh Dinh", email: "minh.dinh4@rmit.edu.vn", phone: "0853453563", imageName: "minh-dinh", locationCoordinate: CLLocationCoordinate2D(latitude: 10.786710386116287, longitude: 106.73818415444727)),
     Contact(name: "Tri Dang", email: "tri.dangtran@rmit.edu.vn", phone: "06786566", imageName: "tri-dang", locationCoordinate: CLLocationCoordinate2D(latitude: 10.79437079611712, longitude: 106.80394039521534)),

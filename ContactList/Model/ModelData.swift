de7e4025403f2d25ee834ef074bbc18ebb54e3fa
Tom Huynh <tomhuynhsg@gmail.com>
2022-07-15 01:48:46 +0700

    Fix the phone number formats

--- ContactList/Model/ModelData.swift
+++ ContactList/Model/ModelData.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 var contacts: [Contact] = [
-    Contact(name: "Tom Huynh", email: "tom.huynh@rmit.edu.vn", phone: "0912325232", imageName: "tom-huynh"),
-    Contact(name: "Brett Kirk", email: "brett.kirk@rmit.edu.vn", phone: "09435325634", imageName: "brett-kirk"),
+    Contact(name: "Tom Huynh", email: "tom.huynh@rmit.edu.vn", phone: "091232522", imageName: "tom-huynh"),
+    Contact(name: "Brett Kirk", email: "brett.kirk@rmit.edu.vn", phone: "094355634", imageName: "brett-kirk"),
     Contact(name: "Minh Dinh", email: "minh.dinh4@rmit.edu.vn", phone: "0853453563", imageName: "minh-dinh"),
-    Contact(name: "Tri Dang", email: "tri.dangtran@rmit.edu.vn", phone: "06786566", imageName: "tri-dang"),
-    Contact(name: "Long Nguyen", email: "long.nguyenminh@rmit.edu.vn", phone: "09624567254", imageName: "long-nguyen"),
-    Contact(name: "Minh Vu", email: "minh.vu@rmit.edu.vn", phone: "0934634534534", imageName: "minh-vu"),
-    Contact(name: "Linh Tran", email: "linh.tranduc@rmit.edu.vn", phone: "0954634745", imageName: "linh-tran"),
+    Contact(name: "Tri Dang", email: "tri.dangtran@rmit.edu.vn", phone: "0674868566", imageName: "tri-dang"),
+    Contact(name: "Long Nguyen", email: "long.nguyenminh@rmit.edu.vn", phone: "0962456754", imageName: "long-nguyen"),
+    Contact(name: "Minh Vu", email: "minh.vu@rmit.edu.vn", phone: "0934634534", imageName: "minh-vu"),
+    Contact(name: "Linh Tran", email: "linh.tranduc@rmit.edu.vn", phone: "095463745", imageName: "linh-tran"),
     Contact(name: "Alberto", email: "alberto@rmit.edu.vn", phone: "094354456", imageName: "alberto"),
     Contact(name: "Cuong Nguyen", email: "cuong.nguyen@rmit.edu.vn", phone: "0922342355", imageName: "cuong-nguyen"),
-    Contact(name: "Khuong Nguyen", email: "khuong.nguyen@rmit.edu.vn", phone: "09453346y3", imageName: "khuong-nguyen"),
+    Contact(name: "Khuong Nguyen", email: "khuong.nguyen@rmit.edu.vn", phone: "09453346323", imageName: "khuong-nguyen"),
     Contact(name: "Luan Nguyen", email: "luan.nguyen@rmit.edu.vn", phone: "0925342356", imageName: "luan-nguyen"),
     Contact(name: "Minh Tran", email: "minh.tran@rmit.edu.vn", phone: "0952534523", imageName: "minh-tran"),
     Contact(name: "Sam Goundar", email: "sam.goundar@rmit.edu.vn", phone: "095534534", imageName: "sam-goundar"),
